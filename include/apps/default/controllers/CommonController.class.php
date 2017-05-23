0b28ca05e17733067ab0b6a6304fc11eb50cdf80
xinlizhang <314625896@qq.com>
2017-05-23 20:02:18 +0800

    aa

--- include/apps/default/controllers/CommonController.class.php
+++ include/apps/default/controllers/CommonController.class.php
@@ -31,16 +31,10 @@ class CommonController extends BaseController
         $this->ecshop_init();
         // 微信oauth处理
         if(class_exists('WechatController')){
-            if (method_exists('WechatController', 'snsapi_userinfo')) {
-                call_user_func(array('WechatController', 'snsapi_userinfo'));
-                
+            if (method_exists('WechatController', 'do_oauth')) {
+                call_user_func(array('WechatController', 'do_oauth'));
             }
         }
-		//是否显示关注按钮
-        $condition['openid'] = !empty($_SESSION['openid']) ? $_SESSION['openid'] : 0;		
-        $userinfo = $this->model->table('wechat_user')->field('subscribe')->where($condition)->find();
-        $this->assign('subscribe', $userinfo['subscribe']);
-
         /* 语言包 */
         $this->assign('lang', L());
         /* 页面标题 */
@@ -48,7 +42,6 @@ class CommonController extends BaseController
         self::$view->assign('page_title', $page_info['title']);
         self::$view->assign('meta_keywords', C('shop_keywords'));
         self::$view->assign('meta_description', C('shop_desc'));
-        C('show_asynclist', 1);
         /* 模板赋值 */
         assign_template();
     }
@@ -68,11 +61,6 @@ class CommonController extends BaseController
         return self::$view;
     }
 
-    protected function fetch($filename, $cache_id = '')
-    {
-        return self::$view->fetch($filename, $cache_id);
-    }
-
     protected function assign($tpl_var, $value = '')
     {
         self::$view->assign($tpl_var, $value);
@@ -87,19 +75,18 @@ class CommonController extends BaseController
     {
         header('Cache-control: private');
         header('Content-type: text/html; charset=utf-8');
-        
+
         $shop_closed = C('shop_closed');
         if (! empty($shop_closed)) {
             $close_comment = C('close_comment');
             $close_comment = empty($close_comment) ? 'closed.':$close_comment;
-            exit('<h1 style="font-size: 5rem;text-align: center;margin-top: 40%;">'.$close_comment.'</h1>');
+            exit($close_comment);
         }
         //NULL
         // 初始化session
-        self::$sess = new EcsSession(self::$db, self::$ecs->table('sessions'), self::$ecs->table('sessions_data'), C('COOKIE_PREFIX').'touch_id');
+        self::$sess = new EcsSession(self::$db, self::$ecs->table('sessions'), self::$ecs->table('sessions_data'), 'ecsid');
         define('SESS_ID', self::$sess->get_session_id());
-
-        if( preg_match('/micromessenger/', strtolower($_SERVER['HTTP_USER_AGENT']))){
+        if( preg_match('/MicroMessenger/i', $_SERVER['HTTP_USER_AGENT'])){
             if(!isset($_SESSION["openid"])||empty($_SESSION["openid"])){//openid为空
                 if(isset($_COOKIE["openid"]) && !empty($_COOKIE["openid"])){
                     $_SESSION["openid"]=$_COOKIE["openid"];
@@ -110,14 +97,13 @@ class CommonController extends BaseController
                 }
             }
         }
-
         // 创建 Smarty 对象
         self::$view = new EcsTemplate();
         self::$view->cache_lifetime = C('cache_time');
         self::$view->template_dir = ROOT_PATH . 'themes/' . C('template');
-        self::$view->cache_dir = ROOT_PATH . 'data/caches/caches';
-        self::$view->compile_dir = ROOT_PATH . 'data/caches/compiled';
-        
+        self::$view->cache_dir = ROOT_PATH . 'data/cache/caches';
+        self::$view->compile_dir = ROOT_PATH . 'data/cache/compiled';
+
         if ((DEBUG_MODE & 2) == 2) {
             self::$view->direct_output = true;
             self::$view->force_compile = true;
@@ -126,7 +112,7 @@ class CommonController extends BaseController
             self::$view->force_compile = false;
         }
         self::$view->caching = true;
-        
+
         // 会员信息
         self::$user = init_users();
         if (empty($_SESSION['user_id'])) {
@@ -143,17 +129,17 @@ class CommonController extends BaseController
                 $_SESSION['discount'] = 1.00;
             }
         }
-        
+
         // 判断是否支持gzip模式
         if (gzip_enabled()) {
             ob_start('ob_gzhandler');
         }
-        
+
         // 设置推荐会员
         if (isset($_GET['u'])) {
             set_affiliate();
         }
-        
+
         // session不存在，检查cookie
         if (! empty($_COOKIE['ECS']['user_id']) && ! empty($_COOKIE['ECS']['password'])) {
             // 找到cookie,验证信息
@@ -174,38 +160,22 @@ class CommonController extends BaseController
                 setcookie("ECS[password]", '', $time, '/');
             }
         }
-        
+
         // search 关键词
         $search_keywords = C('search_keywords');
         if (!empty($search_keywords) && is_string($search_keywords)) {
             $keywords = explode(',', $search_keywords);
             $this->assign('hot_search_keywords', $keywords);
         }
-
-        if (!empty($_COOKIE['ECS']['keywords'])) {
-            $histroy = explode(',',$_COOKIE['ECS']['keywords']);
-            foreach ($histroy as $key=>$val) {
-                if($key < 10){
-                    $histroy_list[$key] = $val;
-                }
-            }
-            $this->assign('search_histroy', $histroy_list);
-        }
-
         // 模板替换
         defined('__TPL__') or define('__TPL__', __ROOT__ . '/themes/' . C('template'));
         $stylename = C('stylename');
         if (! empty($stylename)) {
-            $this->assign('ecs_css_path', __ROOT__ . '/themes/' . C('template') . '/css/style_' . C('stylename') . '.css');
+            $this->assign('ectouch_css_path', __ROOT__ . '/themes/' . C('template') . '/css/ectouch_' . C('stylename') . '.css');
         } else {
-            $this->assign('ecs_css_path', __ROOT__ . '/themes/' . C('template') . '/css/style.css');
+            $this->assign('ectouch_css_path', __ROOT__ . '/themes/' . C('template') . '/css/ectouch.css');
         }
-
-        // 设置parent_id
-        session('parent_id',$_SESSION['user_id'] ? 0 : $_GET['u'] ? $_GET['u'] : 0);
     }
-    
-    
 }
 
 class_alias('CommonController', 'ECTouch');
--- respondwx.php
+++ respondwx.php
@@ -15,13 +15,21 @@
 /* 访问控制 */
 
 
-define('IN_ECTOUCH', true);
-if(!isset($_REQUEST['code'])){
-    //header('location: ./index.php?'.$_SERVER['QUERY_STRING']);
-    //exit;
-}
-$_GET["code"]="YToyOntzOjQ6InR5cGUiO2k6MTtzOjQ6ImNvZGUiO3M6NToid3hwYXkiO30";
-define('CONTROLLER_NAME', 'Respond');
+//define('IN_ECTOUCH', true);
+//if(!isset($_REQUEST['code'])){
+//    //header('location: ./index.php?'.$_SERVER['QUERY_STRING']);
+//    //exit;
+//}
+//$_GET["code"]="YToyOntzOjQ6InR5cGUiO2k6MTtzOjQ6ImNvZGUiO3M6NToid3hwYXkiO30";
+//define('CONTROLLER_NAME', 'Respond');
+//
+///* 加载核心文件 */
+//require ('include/EcTouch.php');
 
-/* 加载核心文件 */
-require ('include/EcTouch.php');
+define('IN_ECTOUCH', true);
+define('CONTROLLER_NAME', 'Respond');
+if(!isset($_REQUEST['code'])){
+    header('location: ./index.php?'.$_SERVER['QUERY_STRING']);
+    exit;
+}
+require ('include/bootstrap.php');
