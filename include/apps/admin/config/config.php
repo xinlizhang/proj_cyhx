51a1f29b70625f6c6f134389cb73becd5e6044c7
xinlizhang <314625896@qq.com>
2017-05-24 20:53:43 +0800

    aa

--- include/apps/admin/config/config.php
+++ include/apps/admin/config/config.php
@@ -2,9 +2,9 @@
 
 return array(
     'APP_STATE' => 1,
-    'APP_NAME' => 'ECTOUCH管理中心',
+    'APP_NAME' => '管理中心',
     'APP_VER' => '1.0.2014.0520',
-    'APP_AUTHOR' => 'ectouch team',
+    'APP_AUTHOR' => 'team',
     'APP_ORIGINAL_PREFIX' => '',
     'APP_TABLES' => '',
 );
